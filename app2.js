2ad3a4952c3a1cef4333f6b8f62b3cc8575fe26d
akashdeeproykarmakar <65176911+akashdeeproykarmakar@users.noreply.github.com>
2020-07-06 03:25:46 +0530

    Update app2.js

--- app2.js
+++ app2.js
@@ -4,6 +4,8 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient  
 const uri = "mongodb+srv://akashkarma:1234@database.q8w8g.gcp.mongodb.net/CreditManagement?retryWrites=true&w=majority";
 const ObjectId = require('mongodb').ObjectID;
+const port = process.env.PORT ;
+
 MongoClient.connect(uri,{ useUnifiedTopology: true })
 .then(client => {
   console.log('Connected to Database')
@@ -56,3 +58,6 @@ app.post('/creditsend', (req, res)=> {
           })
           .catch(error => console.error(error))
     });
+server.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}/`);
+});
